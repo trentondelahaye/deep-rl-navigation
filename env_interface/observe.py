842326989587399b364bdd8623785fa0a2e9d714
Trenton Delahaye <trentondelahaye@hotmail.com>
2021-01-20 14:42:47 +0000

    Simplify watch episode logic

--- env_interface/observe.py
+++ env_interface/observe.py
@@ -7,14 +7,10 @@ def watch_episode(env: UnityEnvironment, agent: Agent, **kwargs):
     brain_name = env.brain_names[0]
     env_info = env.reset(train_mode=False)[brain_name]
     state = env_info.vector_observations[0]
-    score = 0
-    while True:
+    done = env_info.local_done[0]
+    while not done:
         action = agent.act(state)
         env_info = env.step(action)[brain_name]
         next_state = env_info.vector_observations[0]
-        reward = env_info.rewards[0]
         done = env_info.local_done[0]
-        score += reward
         state = next_state
-        if done:
-            break
